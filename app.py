01b3fb55f1515cbfaad8ceaf7d91b11c16129ce8
Kokofreswhat01 <101075714+Kokofreswhat01@users.noreply.github.com>
2023-05-17 17:30:29 +0000

    transaction et overview

--- app.py
+++ app.py
@@ -8,6 +8,15 @@ def count():
      return render_template('count.html')
 
 
+@app.route("/index/overview/")
+def overview():
+     return render_template('HTML/overview.html')
+
+@app.route("/index/transaction/")
+def transaction():
+     return render_template('HTML/transaction.html')
+
+
 if __name__=='__main__':
      app.run(debug=True)
 
