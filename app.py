927b5a9c9d0bca4685e1babfffba133e6884d02d
Kokofreswhat01 <101075714+Kokofreswhat01@users.noreply.github.com>
2023-05-19 17:30:11 +0000

    nouveau job de fre

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/index/")
+@app.route("/index/")
+def index():
+     return render_template('index.html')
+
+
+@app.route("/index/")
 @app.route("/index/count/")
 def count():
      return render_template('count.html')
