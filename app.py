1281e07845752b2d5f654146f6d99b3235c7f069
BENIE SYLVESTRE <sylvestrebenie7829@gmail.com>
2023-05-23 12:57:06 +0000

    mise à niveau de la partie login, add_count, withdrawet register

--- app.py
+++ app.py
@@ -5,8 +5,23 @@ app = Flask(__name__)
 @app.route("/index/")
 @app.route("/index/count/")
 def count():
-     return render_template('count.html')
+     return render_template('HTML/count.html')
 
+@app.route("/index/login/")
+def login():
+     return render_template('HTML/login.html')
+
+@app.route("/index/register/")
+def register():
+     return render_template('HTML/register.html')
+
+@app.route("/index/add_account/")
+def add_count():
+     return render_template('HTML/add_account.html')
+
+@app.route("/index/withdraw/")
+def withdraw():
+     return render_template('HTML/withdraw.html')
 
 if __name__=='__main__':
      app.run(debug=True)
